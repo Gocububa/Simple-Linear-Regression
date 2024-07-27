4936675a301ee0588f97a4904d830816f277d8d8
Burcu Nalbant <117101202+Gocububa@users.noreply.github.com>
2024-07-27 19:21:18 +0300

    Add files via upload

---
+++ 363 hw1.R
@@ -0,0 +1,68 @@
+datam<-read.csv('C:/Users/ASUS/Documents/IceCream.csv',header=TRUE, sep=',')
+
+
+
+# Load necessary libraries
+library(DT)
+
+# Load the dataset
+datam <- read.csv('C:/Users/ASUS/Documents/IceCream.csv', header = TRUE, sep = ',')
+
+# Display the first 10 rows of the dataset in a readable format
+datatable(head(datam, 10), options = list(pageLength = 10), caption = "First 10 Rows of IceCream Dataset")
+
+
+
+x<-datam$Temperature
+y<-datam$Ice.Cream.Profits
+
+my_reg<-lm(y~x,datam)
+
+#linearity
+library(ggplot2)
+ggplot(datam,aes(x=x,y=y))+
+  geom_point()+
+  geom_smooth(method="lm", col="red") 
+
+#independence of errors
+#normality of errors
+#equal variances
+par(mfrow=c(2,2))
+plot(my_reg)
+
+
+
+library(corrplot)
+
+
+# Select numeric columns only
+numeric_columns <- datam[sapply(datam, is.numeric)]
+
+# Calculate the correlation matrix
+correlation_matrix <- cor(numeric_columns, use = "complete.obs")
+
+# Print the correlation matrix
+print(correlation_matrix)
+
+# Visualize the correlation matrix using corrplot
+corrplot(correlation_matrix, method = "circle", type = "upper", tl.col = "black", tl.srt = 45, addCoef.col = "black")
+
+
+
+#for r-squared and regression equation
+summary(my_reg)
+
+#for anova and hypothesis testing
+anova(my_reg)
+
+
+
+
+
+
+
+library(tidyverse)
+data |> ggplot(aes(x,y)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
